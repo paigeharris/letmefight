9b3dded81ac7dbcf3a4b8a44c3bf992fbd621d4f
Paige Harris <31494725+paigeharris@users.noreply.github.com>
2023-10-23 00:25:07 -0700

    Add files via upload

---
+++ BuyTroops/LetPlayerFight.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameMenus;
+using TaleWorlds.Library;
+
+namespace LetMeFight
+{
+    public class LetPlayerFight : CampaignBehaviorBase
+    {
+        private CampaignGameStarter _obj = (CampaignGameStarter)null;
+        private string _menuOptionId = "LetMeFight";
+
+        public void debugMsg(string message) => InformationManager.DisplayMessage(new InformationMessage(message));
+
+        public void addFightOption(string menuId) => this._obj.AddGameMenuOption(menuId, this._menuOptionId, "Let Me Fight!", (GameMenuOption.OnConditionDelegate)(grr =>
+        {
+            grr.optionLeaveType = GameMenuOption.LeaveType.Raid;
+            return Hero.MainHero.IsWounded;
+        }), (GameMenuOption.OnConsequenceDelegate)(grr =>
+        {
+            if (Hero.MainHero.HitPoints > 20)
+                return;
+            Hero.MainHero.HitPoints = 21;
+            GameMenu.ActivateGameMenu(menuId);
+        }), true, 1, true);
+
+        public void letPlayerFightFunc(CampaignGameStarter obj)
+        {
+            this.addFightOption("encounter");
+            this.addFightOption("menu_siege_strategies");
+        }
+
+        public void OnSessionLaunched(CampaignGameStarter obj)
+        {
+            this._obj = obj;
+            this.letPlayerFightFunc(this._obj);
+        }
+
+        public override void RegisterEvents() => CampaignEvents.OnSessionLaunchedEvent.AddNonSerializedListener((object)this, new Action<CampaignGameStarter>(this.OnSessionLaunched));
+
+        public override void SyncData(IDataStore dataStore)
+        {
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,29 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace LetMeFight
+{
+    public class Main : MBSubModuleBase
+    {
+        private LetPlayerFight _letPlayerFightBehavior = (LetPlayerFight)null;
+        public string menuOptionId = "LetMeFight";
+
+        protected override void OnSubModuleLoad()
+        {
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            if (!(game.GameType is Campaign))
+                return;
+            this.AddBehaviors((CampaignGameStarter)gameStarterObject);
+        }
+
+        private void AddBehaviors(CampaignGameStarter gameIntializer)
+        {
+            this._letPlayerFightBehavior = new LetPlayerFight();
+            gameIntializer.AddBehavior((CampaignBehaviorBase)this._letPlayerFightBehavior);
+        }
+    }
+}
